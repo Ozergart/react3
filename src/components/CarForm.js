8186d80de85370e807bc3b869a81231baf85545c
Kiril <ozergart@gmail.com>
2024-02-09 19:22:26 +0200

    hw3.3

--- src/components/CarForm.js
+++ src/components/CarForm.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {useForm} from "react-hook-form";
 import {PostService} from "../services/PostServ";
+import axios from "axios";
 
 const CarForm = () => {
     const {register,reset,handleSubmit} = useForm()
 const save = (car) =>{
     PostService.post(car).then(({data})=> console.log(data))
+
 }
     return (
         <div>
