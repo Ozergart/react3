005b479c233aabe47147deb64332b3b5ada20c4a
Kiril <ozergart@gmail.com>
2024-02-21 11:22:01 +0200

    hw4

--- src/components/episodCont/Episodes.js
+++ src/components/episodCont/Episodes.js
@@ -3,11 +3,25 @@ import React, {useEffect, useState} from 'react';
 import css from './Episodes.module.css'
 import {episodeService} from "../../services";
 import {Episode} from "./Episode";
+import {useSearchParams} from "react-router-dom";
 
 const Episodes = () => {
 
     const [episodes, setEpisodes] = useState([])
     const [prevNext, setPrevNext] = useState({prev:null,next:null})
+    const [query, setQuery] = useSearchParams({page:"1"})
+    const prev =()=>{
+        setQuery(prev=> {
+            prev.set("page", `${+prev.get('page')-1}`)
+            return prev
+        })
+    }
+    const next =()=>{
+        setQuery(next=>{
+            next.set('page', `${+next.get('page')+1}`)
+            return next
+        })
+    }
 
     useEffect(() => {
         episodeService.getAll().then(({data})=> {
@@ -21,8 +35,8 @@ const Episodes = () => {
         <div className={css.Episodes}>
             {episodes.map(episod=><Episode key={episod.id} episod={episod}/>)}
             <div className={css.buttonsCont}>
-            <button disabled={!prevNext.prev}>prev</button>
-            <button disabled={!prevNext.next}>next</button>
+            <button disabled={!prevNext.prev} onClick={prev}>prev</button>
+            <button disabled={!prevNext.next} onClick={next}>next</button>
             </div>
         </div>
     );
