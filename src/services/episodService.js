f0b8e8575f63bda8f4dfcb0e9cff65ef5d3b8c46
Kiril <ozergart@gmail.com>
2024-02-21 11:24:29 +0200

    hw4

--- src/services/episodService.js
+++ src/services/episodService.js
@@ -2,7 +2,7 @@ import {apiService} from "./apiService";
 import {urls} from "../constants";
 
 const episodeService = {
-    getAll: (page='1')=> apiService.get(urls.episodes, params),
+    getAll: (page='1')=> apiService.get(urls.episodes, params:{{page}}),
 }
 export {
     episodeService
